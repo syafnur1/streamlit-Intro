5c3a1d2dc1ddd17708166d2a17661d3e837375cb
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-11 20:18:42 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from PIL import Image
 
 st.set_page_config(layout="wide")

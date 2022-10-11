25bb46613259bb200fb68bc4011ff614cd1ec495
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-11 20:22:57 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -18,17 +18,20 @@ st.code("import streamlit as st")
 # Deklarasi dataset
 df = pd.read_csv('store - store.csv')
 
+# Data prep
+
+# End of Data Prep
 st.dataframe(df)
+
+# metrics
+st.metric("Total Sales", 1000, 10)
+
+st.metric("Total Profit", "$ 10M", "-2.3%")
+
+st.title("Charting")
+
+
 
 st.caption('Ini caption')
 
-
 st.line_chart(['Quantity'])
-
-
-
-
-
-
-
-

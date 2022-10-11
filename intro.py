10edc28f64be9e34cd2365ad978e37baa9af29cb
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-11 20:17:27 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd
-
-df = pd.read_csv('store - store.csv')
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
 
 st.set_page_config(layout="wide")
 
-st.title("Dashboar Latihan")
-
 st.write("Nama Saya Zul")
 
+st.markdown("Nama saya **Rizki**")
+st.markdown("---")
+
+st.title("Dashboar Latihan")
 st.header("Ini header")
+
+st.code("import streamlit as st")
+
+# Deklarasi dataset
+df = pd.read_csv('store - store.csv')
 
 st.dataframe(df)
 

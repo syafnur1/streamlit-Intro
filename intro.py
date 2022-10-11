116ec422321a737f38ba386ff091fcf240192d7b
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-11 19:45:51 +0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+# df = pd.read_csv('/Users/muthiatriani/Documents/TETRIS PROGRAME/TETRIS Python Vid/store - store.csv')
+
+st.set_page_config(layout="wide")
+
+st.title("Dashboar Latihan")
+
+st.write("Nama Saya Zul")
+
+st.header("Ini header")
+
+# st.dataframe(df)
+
+st.caption('Ini caption')
+
+
+st.line_chart(['Quantity'])
+
+
+
+
+
+
+
+

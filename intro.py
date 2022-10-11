7ff0f23e90dc3cef566feecb2b78bcfcf34e3336
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-11 20:28:03 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -32,5 +32,4 @@ st.title("Charting")
 
 with st.sidebar:
     st.title("Dashboard store")
-    freq = st.selectbox("Masukkan frekuensi", ('D', 'W', 'M', 'Q', 'Y'))
-    with st.expander("Ketahui lebih lanjut..."):
+    freq = st.selectbox("Masukkan frekuensi", ('D', 'W', 'M', 'Q', 'Y')):

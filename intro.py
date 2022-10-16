1c11a39454f178d004f10054b813b525affe2318
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-17 00:27:47 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -4,6 +4,7 @@ import numpy as np
 #import seaborn as sns
 #import matplotlib.pyplot as plt
 from PIL import Image
+import lorem
 
 #from sklearn.linear_model import LinearRegression
 #from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score

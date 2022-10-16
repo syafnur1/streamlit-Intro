6fd2ed6c1569a0e050772d3fa9d83274e7cf32c8
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-17 00:53:58 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -43,6 +43,10 @@ st.dataframe(df)
 df1= df.iloc[0:10]
 st.line_chart(data=df1, x = 'Ship Date', y = 'Profit', width=0, height=0, use_container_width=True)
 
+
+st.bar_chart(df1)
+
+
 # metrics
 st.metric("Total Sales", 1000, 10)
 
@@ -54,7 +58,24 @@ with st.sidebar:
     st.title("Dashboard store")
     freq = st.selectbox("Masukkan frekuensi", ('D', 'W', 'M', 'Q', 'Y'));
 
-    
+import plotly.figure_factory as ff
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)   
  
     
 '''

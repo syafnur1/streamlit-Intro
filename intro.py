232bff46ff58beb867cb5fbf92a9a395fcb3f4c6
syafnur1 <115526769+syafnur1@users.noreply.github.com>
2022-10-17 01:14:36 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -58,7 +58,14 @@ with st.sidebar:
     st.title("Dashboard store")
     freq = st.selectbox("Masukkan frekuensi", ('D', 'W', 'M', 'Q', 'Y'));
 
-
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
  
     
 '''
